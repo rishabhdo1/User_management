91eaad20cc316fb0c471f75bd94662086a0836b7
rishabhdo1 <rishabhdobhal19@gmail.com>
2026-02-18 12:23:38 +0530

    second commit

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ require("dotenv").config();
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", async (req, res) => {
+    res.send("<h1>Welcome to Backend</h1>");
+  });
 app.listen(PORT, ()=>{
     console.log(`Server is running on port ${PORT}`);
 });
